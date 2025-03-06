8304d3015a2b76df68aaca16aab02e920c1a8bc6
23031702 <181597497+23031702@users.noreply.github.com>
2025-03-06 22:00:12 +0200

    Add files via upload

---
+++ RectangleCalculations.java
@@ -0,0 +1,14 @@
+public class RectangleCalculations {
+    public static void main(String[] args) {
+        
+        double length = 16; 
+        double width = 8;  
+        
+      
+        System.out.println("Area of the rectangle: " + area);
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+    }
+}
+
+
+
